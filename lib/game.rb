a014597e6fde043e6136351006c4bc121664a025
Santiago Guerra <guerrasantiago19@gmail.com>
2019-11-04 11:12:19 -0500

    GAME CLASS create detect_winner method, this method will detect a winner and will return his/her name.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 load 'player.rb'
+load 'board.rb'
 
 class Game
   # rubocop:disable Metrics/LineLength
@@ -9,5 +10,21 @@ class Game
   # rubocop:enable Metrics/LineLength
   def running?(player = nil)
     player.is_winner
+  end
+
+  def detect_winner(player, board)
+    current_board = board.render
+    WINNING_COMBINATIONS.each do |line_winner|
+      a = line_winner[0]
+      b = line_winner[1]
+      c = line_winner[2]
+      next unless (current_board[a] == current_board[b]) && (current_board[b] == current_board[c])
+
+      if current_board[c] == player.piece
+        player.is_winner = true
+        running? player
+      end
+    end
+    player.name if player.is_winner
   end
 end
