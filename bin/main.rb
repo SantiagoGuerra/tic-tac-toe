a66bbb0d73dc17b98e537f92819ec1766bdd21d3
Santiago Guerra <guerrasantiago19@gmail.com>
2019-11-05 11:41:02 -0500

    MAIN FILE add basic user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,11 @@
+
 # frozen_string_literal: true
 
 # !/usr/bin/env ruby
+require_relative '../lib/player.rb'
+require_relative '../lib/board.rb'
+require_relative '../lib/game.rb'
+
 
 puts "Hello World\n\n"
 
@@ -9,47 +14,51 @@ puts "This is Tic tac toe game.\n\n"
 
 # Ask for players' info
 print 'Player 1 what is your name? '
-player_one = gets.chomp
-puts "Thank you: #{player_one}\n\n"
+player_one_name = gets.chomp
+player_one = Player.new(player_one_name)
+puts "Thank you: #{player_one.name}\n\n"
+
+
+
 print 'Player 2 what is yours '
-player_two = gets.chomp
-puts "Thank you: #{player_two}\n\n"
-print "#{player_one} do you choose X or O ? "
+player_two_name = gets.chomp
+player_two = Player.new(player_two_name)
+puts "Thank you: #{player_two.name}\n\n"
+
+
+print "#{player_one.name} do you choose X or O ? "
 player_one_chip = gets.chomp
+player_one.piece = player_one_chip
 puts "#{player_one} chose: #{player_one_chip}\n\n"
+
+
 player_two_chip = player_one_chip == 'X' ? 'O' : 'X'
 puts "#{player_two} yours are: #{player_two_chip}\n\n"
+player_two.piece = player_two_chip
 
 # Some instructions
 puts "Whoever gets a straight line wins. Lets start!\n\n"
 
 # Game play
-puts "#{player_one} choose one number"
-puts '1|2|3'
-puts '-+-+-'
-puts '4|5|6'
-puts '-+-+-'
-puts '7|8|9'
-player_1_choose = gets.chomp
-puts "you chose: #{player_1_choose}"
-puts "#{player_two} your turn"
-puts '1|2|3'
-puts '-+-+-'
-puts '4|X|6'
-puts '-+-+-'
-puts '7|8|9'
-player_2_choose = gets.chomp
-puts "you chose: #{player_2_choose}"
-puts "#{player_one} choose one number"
-puts '1|2|3'
-puts '-+-+-'
-puts '4|X|6'
-puts '-+-+-'
-puts "7|8|O\n\n"
 
-puts "And so on and so on\n\n"
+game = Game.new(player_one, player_two, Board.new)
+loop do
+  cells = game.board.render
 
-# Win messages
-puts "#{player_one} wins"
-puts "#{player_two} wins"
-puts 'Nobody wins'
+  puts "#{game.current_player.name} choose a number available on the board."
+
+  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+  puts " - + - + - "
+  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+  puts " - + - + - "
+  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+
+  game.play(gets.chomp.to_i)
+
+  break if game.running?
+end
+
+
+puts "Congrulations #{game.current_player.name}! you won." if game.current_player.is_winner
+
+puts "Nobody wins" unless game.not_a_tie?
