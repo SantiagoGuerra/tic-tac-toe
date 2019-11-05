bc83516b11c34a96a9a8f6f6aa1eccd12e4bcab6
Santiago Guerra <guerrasantiago19@gmail.com>
2019-11-05 15:01:40 -0500

    MAIN.RB FILE add interface to user and improve validation system.

--- bin/main.rb
+++ bin/main.rb
@@ -1,40 +1,40 @@
-
 # frozen_string_literal: true
 
 # !/usr/bin/env ruby
 require_relative '../lib/player.rb'
 require_relative '../lib/board.rb'
 require_relative '../lib/game.rb'
-
-
-puts "Hello World\n\n"
+require_relative '../lib/utils.rb'
 
 # Welcome message
 puts "This is Tic tac toe game.\n\n"
 
 # Ask for players' info
-print 'Player 1 what is your name? '
+print 'Player 1 - What is your name?: '
 player_one_name = gets.chomp
 player_one = Player.new(player_one_name)
 puts "Thank you: #{player_one.name}\n\n"
 
-
-
-print 'Player 2 what is yours '
+print 'Player 2 - What is yours?: '
 player_two_name = gets.chomp
 player_two = Player.new(player_two_name)
 puts "Thank you: #{player_two.name}\n\n"
 
+print "#{player_one.name} do you choose X or O ?: "
 
-print "#{player_one.name} do you choose X or O ? "
-player_one_chip = gets.chomp
-player_one.piece = player_one_chip
-puts "#{player_one} chose: #{player_one_chip}\n\n"
+player_one_chip = gets.chomp.to_sym
 
+validate_chip(player_one, player_one_chip)
 
-player_two_chip = player_one_chip == 'X' ? 'O' : 'X'
-puts "#{player_two} yours are: #{player_two_chip}\n\n"
+puts "#{player_one.name} chose: #{player_one_chip}\n\n"
+
+player_two_chip = player_one_chip == :X ? :O : :X
+
 player_two.piece = player_two_chip
+
+puts "#{player_two.name}, you'll use #{player_two.piece} \n\n\n"
+
+puts "- - - - - - - - - - - - - - - - - - - - - - \n\n"
 
 # Some instructions
 puts "Whoever gets a straight line wins. Lets start!\n\n"
@@ -43,22 +43,18 @@ puts "Whoever gets a straight line wins. Lets start!\n\n"
 
 game = Game.new(player_one, player_two, Board.new)
 loop do
-  cells = game.board.render
+  puts render_board(game.board.render)
 
-  puts "#{game.current_player.name} choose a number available on the board."
+  print "#{game.current_player.name} choose a number available on the board: "
+  selected_num = validate_selection(gets.chomp.to_i, game.board.render)
 
-  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-  puts " - + - + - "
-  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-  puts " - + - + - "
-  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
-
-  game.play(gets.chomp.to_i)
+  game.play(selected_num)
 
   break if game.running?
 end
 
+puts render_board(game.board.render)
 
 puts "Congrulations #{game.current_player.name}! you won." if game.current_player.is_winner
 
-puts "Nobody wins" unless game.not_a_tie?
+puts 'Nobody win' unless game.not_a_tie?
