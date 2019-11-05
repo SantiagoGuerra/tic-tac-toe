102847db503ee17869f4fc63d40a0491b2b9b5e6
euqueme <euqueme@gmail.com>
2019-11-05 09:59:56 -0600

    BOARD AND GAME
    Make the Board class independent by not loading the player in the change_cell method
    
    Create initialize, play, next_to_play and not_a_tie methods to switch between players and also validate if the game is a tie in the Game class
    
    optimize  detect winner  and modify running methods to test the Game class.
    
    The code passes all rubocops.

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-load 'player.rb'
-
 class Board
   def initialize
     @game_board = (1..9).to_a
   end
 
-  def change_cell(player = nil)
-    @game_board[(player.cell_selected - 1)] = player.piece if change_error?((player.cell_selected - 1))
+  def change_cell(cell_selected = nil, piece = nil)
+    @game_board[(cell_selected - 1)] = piece if change_error?((cell_selected - 1))
     render
   end
 
